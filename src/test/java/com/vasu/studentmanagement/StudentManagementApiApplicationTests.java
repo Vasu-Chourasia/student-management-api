8f85e2f3e768fa5116ccc2cfbab4172cabf97e9d
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-08-08 00:25:49 +0530

    Initial Spring Boot Project Setup

---
+++ src/test/java/com/vasu/studentmanagement/StudentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasu.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
