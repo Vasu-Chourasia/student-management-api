fdeeba17182423a883a0d3a5022ab7dd5bbfb2ee
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-08-10 18:44:33 +0530

    updated CORS configurations

--- src/main/java/com/vasu/studentmanagement/config/CorsConfig.java
+++ src/main/java/com/vasu/studentmanagement/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://student-management-frontend-jrps.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
